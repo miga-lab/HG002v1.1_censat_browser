9c5344d4904cdf6c1169391f4d100c371d89bbfc
jmenendez98 <jmmenend@ucsc.edu>
2024-11-08 09:10:46 -0800

    updated CDR tracks

--- hub/track_builds/CDR/CDR_build.sh
+++ hub/track_builds/CDR/CDR_build.sh
@@ -9,7 +9,7 @@ bedGraphToBigWig \
 	HG002v1.1_ONT_hmmCDR_scores.bigwig
 bedToBigBed \
     -type=bed9 \
-    -as=hg002v1.1_ont_hmmCDR.as \
+    -as=autosql/hg002v1.1_ont_hmmCDR.as \
     HG002v1.1_ONT_hmmCDR.bed \
 	/public/groups/migalab/HG002v1.1_censat_browser/hub/hg002v1.1.fasta.2bit.sizes \
 	HG002v1.1_ONT_hmmCDR.bb
@@ -18,7 +18,7 @@ awk '{ $5 = int($5); OFS="\t"; print }' HG002v1.1_ONT_hmmCDR_subCDR.bed > temp &
     mv temp HG002v1.1_ONT_hmmCDR_subCDR.bed 
 bedToBigBed \
     -type=bed9 \
-    -as=hg002v1.1_ont_hmmCDR_subCDR.as \
+    -as=autosql/hg002v1.1_ont_hmmCDR_subCDR.as \
     HG002v1.1_ONT_hmmCDR_subCDR.bed \
 	/public/groups/migalab/HG002v1.1_censat_browser/hub/hg002v1.1.fasta.2bit.sizes \
 	HG002v1.1_ONT_hmmCDR_subCDR.bb
@@ -26,7 +26,7 @@ sort -k1,1 -k2,2n HG002v1.1_ONT_hmmCDR_priorCDR.bed > temp && \
     mv temp HG002v1.1_ONT_hmmCDR_priorCDR.bed
 bedToBigBed \
     -type=bed3 \
-    -as=hg002v1.1_ont_hmmCDR_priors.as \
+    -as=autosql/hg002v1.1_ont_hmmCDR_priors.as \
     HG002v1.1_ONT_hmmCDR_priorCDR.bed \
 	/public/groups/migalab/HG002v1.1_censat_browser/hub/hg002v1.1.fasta.2bit.sizes \
 	HG002v1.1_ONT_hmmCDR_priorCDR.bb
@@ -38,7 +38,7 @@ bedGraphToBigWig \
 	HG002v1.1_HiFi_hmmCDR_scores.bigwig
 bedToBigBed \
     -type=bed9 \
-    -as=hg002v1.1_hifi_hmmCDR.as \
+    -as=autosql/hg002v1.1_hifi_hmmCDR.as \
     HG002v1.1_HiFi_hmmCDR.bed \
 	/public/groups/migalab/HG002v1.1_censat_browser/hub/hg002v1.1.fasta.2bit.sizes \
 	HG002v1.1_HiFi_hmmCDR.bb
@@ -47,7 +47,7 @@ awk '{ $5 = int($5); OFS="\t"; print }' HG002v1.1_HiFi_hmmCDR_subCDR.bed > temp 
     mv temp HG002v1.1_HiFi_hmmCDR_subCDR.bed
 bedToBigBed \
     -type=bed9 \
-    -as=hg002v1.1_hifi_hmmCDR_subCDR.as \
+    -as=autosql/hg002v1.1_hifi_hmmCDR_subCDR.as \
     HG002v1.1_HiFi_hmmCDR_subCDR.bed \
 	/public/groups/migalab/HG002v1.1_censat_browser/hub/hg002v1.1.fasta.2bit.sizes \
 	HG002v1.1_HiFi_hmmCDR_subCDR.bb
@@ -55,7 +55,7 @@ sort -k1,1 -k2,2n HG002v1.1_HiFi_hmmCDR_priorCDR.bed > temp && \
     mv temp HG002v1.1_HiFi_hmmCDR_priorCDR.bed
 bedToBigBed \
     -type=bed3 \
-    -as=hg002v1.1_hifi_hmmCDR_priors.as \
+    -as=autosql/hg002v1.1_hifi_hmmCDR_priors.as \
     HG002v1.1_HiFi_hmmCDR_priorCDR.bed \
 	/public/groups/migalab/HG002v1.1_censat_browser/hub/hg002v1.1.fasta.2bit.sizes \
 	HG002v1.1_HiFi_hmmCDR_priorCDR.bb
